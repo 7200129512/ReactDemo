24aa366d95ff1ac34bff84b381eaf1286ec262b1
7200129512 <35841959+7200129512@users.noreply.github.com>
2022-01-23 12:49:46 +0530

    node module added

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//import Home from './App'
+import ReactDOM  from 'react-dom'
+import About from './about'
+import Userform  from './userform'
+
+
+// ReactDOM.render(<Home address="karaikudi" doorno="41" phno="7200129512"/>,document.getElementById('root'));
+//ReactDOM.render(<About address="karaikudi" doorno="41" email="sara9566@gmail.com"/>,document.getElementById('root'));
+
+ReactDOM.render(<Userform/>,document.getElementById('root'));
